d5cc118903e87376082267d46ea33369e14cf940
Hayato Takahashi <hayatostar1025@gmail.com>
2025-11-02 21:37:59 +0900

    デプロイ用資材

--- page/vite.config.ts
+++ page/vite.config.ts
@@ -4,11 +4,17 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/maus-hub/", // GitHub Pagesのベースパス
     server: {
         headers: {
             // COOP対策：ポップアップを許可する設定
             "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
             "Cross-Origin-Embedder-Policy": "unsafe-none",
         },
+    },
+    build: {
+        outDir: "dist",
+        sourcemap: false,
+        assetsDir: "assets",
     },
 });
